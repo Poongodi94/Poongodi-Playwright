cf640c4a179680495f264a5e9b1b7329025719e0
Poongodi94 <Poongodismail20@gmail.com>
2025-09-18 17:43:41 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,9 @@
+var firstname = `Pooja`
+console.log(typeof firstname);
+var companyname = 'virtusa'
+console.log(typeof companyname);
+var mobilenumber = 9884566889
+console.log(typeof mobilenumber);
+var isAutomation = false
+console.log(typeof isAutomation);
+
