c5b107b4bda443bbcfdb48f40032e0c91b5bc6b3
Poongodi94 <Poongodismail20@gmail.com>
2025-10-02 05:59:18 +0530

    Add files via upload

---
+++ Twosum.js
@@ -0,0 +1,18 @@
+function Pair(nums, target) 
+{
+   let sum = [] 
+   for (let i = 0;  i< nums.length; i++) {  
+    for (let j = i+1;  j< nums.length; j++) {
+       if (nums[i]+nums[j]===target) {
+        sum.push([i, j])
+        
+       }
+       
+    }
+    
+   }
+   return sum 
+}
+const nums = [2, 4, 7, 8, 11, 14];
+const target = 18;
+console.log(Pair(nums, target));
